b9d7b97dcb6eec70eb042b11719e523b1669250c
Pedro Matiello <pedro@pmatiello.me>
2025-02-03 22:38:41 -0300

    Add docstrings to mockfn.matchers.

--- src/me/pmatiello/mockfn/matchers.clj
+++ src/me/pmatiello/mockfn/matchers.clj
@@ -3,10 +3,14 @@
 
 (defrecord Matcher [name function expected args-desc-fn])
 
-(defn matches? [matcher actual]
+(defn matches?
+  "Returns whether the matcher accepts the actual value."
+  [matcher actual]
   ((:function matcher) actual (:expected matcher)))
 
-(defn description [matcher]
+(defn description
+  "Describes a matcher by returning it's name and expectations."
+  [matcher]
   (let [m-name       (:name matcher)
         args-desc-fn (:args-desc-fn matcher)
         expected     (-> matcher :expected args-desc-fn)]
@@ -15,20 +19,31 @@
          (str/join " "))))
 
 (defn make
+  "Produces a new matcher instance."
   ([name function expected] (make name function expected identity))
   ([name function expected description-fn] (->Matcher name function expected description-fn)))
 
-(defn exactly [expected]
+(defn exactly
+  "Returns a matcher that expects an exact value."
+  [expected]
   (make "exactly" = expected))
 
-(defn at-least [expected]
+(defn at-least
+  "Returns a matcher that expects a value greater than or equal to the argument."
+  [expected]
   (make "at least" >= expected))
 
-(defn at-most [expected]
+(defn at-most
+  "Returns a matcher that expects a value less than or equal to the argument."
+  [expected]
   (make "at most" <= expected))
 
-(defn any []
+(defn any
+  "Returns a matcher that expects any value."
+  []
   (make "any" (constantly true) nil))
 
-(defn a [expected]
+(defn a
+  "Returns a matcher that expects an instance of the provided class."
+  [expected]
   (make "a" #(instance? %2 %1) expected pr-str))
